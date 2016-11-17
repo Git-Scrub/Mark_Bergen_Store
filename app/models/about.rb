644678a20e7a8057912e561d27f4bbbb0c0aca95
Lionel Bergen <LionelBergen@live.com>
2016-11-17 03:28:15 -0600

    Added validation to the about table

--- app/models/about.rb
+++ app/models/about.rb
@@ -1,8 +1,10 @@
+class AboutValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:base] << "You should only have one Abouts description" if About.count > 0 || About.first == self;
+  end
+end
+
 class About < ApplicationRecord
   validates :about_us_description, presence: true
-  
-  private
-  def there_can_only_be_one
-    errors[:base] << 'There can only be one' if About.count > 0 || About.first == self;
-  end
+  validates_with AboutValidator
 end
