478ecdea972dfa77e81649d1b33a801dc4f39f51
Lionel Bergen <LionelBergen@live.com>
2016-11-17 18:17:52 -0600

    Added Store Controller And added a route to the store application from the index page

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,16 @@
+class StoreController < ApplicationController
+  def home
+  end
+
+  def about_us
+  end
+
+  def FAQ
+  end
+
+  def search
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'store#home'
+  get 'store/home'
+
+  get 'store/about_us'
+
+  get 'store/FAQ'
+
+  get 'store/search'
+
+  get 'store/contact'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
