f350ab010eac1c00849a716f78e486984b75d76d
Lionel Bergen <LionelBergen@live.com>
2016-11-18 05:52:24 -0600

    Changed secrets for linux support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  
+  # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
+config.assets.precompile += ['active_admin.css.scss', 'active_admin.js'] 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
