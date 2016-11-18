edf860ce9183ab028a3f60887b456dfb9c9e6720
Lionel Bergen <LionelBergen@live.com>
2016-11-18 01:16:31 -0600

    Added validation to the contact module

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -1,10 +1,9 @@
-class ContactInfoValidator < ActiveModel::Validator
-  def validate(record)
-    record.errors[:base] << "You should only have one Contact us page description" if ContactInfo.count > 0 || ContactInfo.first == self;
-  end
-end
-
 class ContactInfo < ApplicationRecord
   validates :contact_page_text, presence: true
-  validates_with ContactInfoValidator
+  validate :validate_allow_one_record_only
+  
+  def validate_allow_one_record_only
+    max_records = new_record? ? 0 : 1
+    errors[:base] << "You should only have one Abouts description" if About.count > max_records
+  end
 end
