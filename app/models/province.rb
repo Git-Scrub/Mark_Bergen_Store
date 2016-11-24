15bda5fc30f9279a8caf45c53a27c50766144528
Lionel Bergen <LionelBergen@live.com>
2016-11-24 02:12:46 -0600

    Added province validation

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,7 @@
 class Province < ApplicationRecord
+   validates :name, :tax_rate, :province_code, presence: true, allow_blank: false
+   
+   validates_format_of :name, :with => /\A[a-zA-Z]*\Z/
+   validates_format_of :province_code, :with => /\A[a-zA-Z]*\Z/
+   validates_length_of :province_code, :is => 2
 end
